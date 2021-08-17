21451c88f3b5deb00c9e64606a3501d58b424073
Hendrik Peter van der Meulen <peter@picoreclame.nl>
2021-08-17 16:44:04 +0200

    Add reviews, review comments, commits and pull req meta

--- lib/github.rb
+++ lib/github.rb
@@ -9,6 +9,17 @@ class Github
     issues.each do |issue|
       issue['comments'] = (issue['comments'] >= 1) ? Github.fetch_all_meta(issue['comments_url']) : []
       issue['events'] = Github.fetch_all_meta(issue['events_url'])
+
+      if issue['pull_request']
+        pull_request_meta = issue['pull_request']
+        issue['pull_request'] = Github.fetch_all_meta(pull_request_meta['url'])
+        puts issue['pull_request']['review_comments_url']
+        issue['pull_request']['reviews'] = Github.fetch_all_meta("#{GITHUB_URL}/repos/#{repo}/pulls/#{issue['number']}/reviews")
+        issue['pull_request']['review_comments'] = Github.fetch_all_meta(issue['pull_request']['review_comments_url'])
+        issue['pull_request']['commits'] = Github.fetch_all_meta(issue['pull_request']['commits_url'])
+        issue['pull_request'].delete('head')
+        issue['pull_request'].delete('base')
+      end
     end
 
     issues
@@ -31,9 +42,11 @@ class Github
     end
   end
 
-  def self.fetch_all_meta(url)
+  def self.fetch_all_meta(url, json = true)
       response = Github.fetch_meta(url)
       body = response.body.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
+      return body unless json
+
       parsed = JSON.parse(body)
   end
 
@@ -49,7 +62,7 @@ class Github
 
   def self.fetch_meta(url)
     response = HTTParty.get(
-      url,
+      "#{url}?per_page=100",
       headers: {
         Accept: 'application/vnd.github.v3+json"',
         Authorization: "token #{GITHUB_TOKEN}"
