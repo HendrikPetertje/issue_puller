f6da72321c8cfd44da2cd0714caccd7fd764650c
peter van der Meulen <peter@hendrikpeter.net>
2021-08-18 16:48:51 +0200

    Implement retry logic

--- lib/github.rb
+++ lib/github.rb
@@ -3,23 +3,23 @@ class Github
   GITHUB_TOKEN = ENV.fetch('GITHUB_TOKEN')
   GITHUB_URL = ENV.fetch('GITHUB_URL', 'https://api.github.com')
 
-
   def self.fetch_all(repo)
     issues = Github.fetch_all_issues(repo)
     issues.each do |issue|
-      issue['comments'] = (issue['comments'] >= 1) ? Github.fetch_all_meta(issue['comments_url']) : []
+      issue['comments'] = issue['comments'] >= 1 ? Github.fetch_all_meta(issue['comments_url']) : []
       issue['events'] = Github.fetch_all_meta(issue['events_url'])
 
-      if issue['pull_request']
-        pull_request_meta = issue['pull_request']
-        issue['pull_request'] = Github.fetch_all_meta(pull_request_meta['url'])
-        puts issue['pull_request']['review_comments_url']
-        issue['pull_request']['reviews'] = Github.fetch_all_meta("#{GITHUB_URL}/repos/#{repo}/pulls/#{issue['number']}/reviews")
-        issue['pull_request']['review_comments'] = Github.fetch_all_meta(issue['pull_request']['review_comments_url'])
-        issue['pull_request']['commits'] = Github.fetch_all_meta(issue['pull_request']['commits_url'])
-        issue['pull_request'].delete('head')
-        issue['pull_request'].delete('base')
-      end
+      next unless issue['pull_request']
+
+      puts "meta data fetch for #{repo}/pulls/#{issue['number']}"
+      pull_request_meta = issue['pull_request']
+      issue['pull_request'] = Github.fetch_all_meta(pull_request_meta['url'])
+      issue['pull_request']['reviews'] =
+        Github.fetch_all_meta("#{GITHUB_URL}/repos/#{repo}/pulls/#{issue['number']}/reviews")
+      issue['pull_request']['review_comments'] = Github.fetch_all_meta(issue['pull_request']['review_comments_url'])
+      issue['pull_request']['commits'] = Github.fetch_all_meta(issue['pull_request']['commits_url'])
+      issue['pull_request'].delete('head')
+      issue['pull_request'].delete('base')
     end
 
     issues
@@ -42,16 +42,15 @@ class Github
     end
   end
 
-  def self.fetch_all_meta(url, json = true)
-      response = Github.fetch_meta(url)
-      body = response.body.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
-      return body unless json
+  def self.fetch_all_meta(url)
+    response = Github.fetch_meta(url)
+    body = response.body.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
 
-      parsed = JSON.parse(body)
+    parsed = JSON.parse(body)
   end
 
   def self.fetch_page(repo, page)
-    response = HTTParty.get(
+    HTTParty.get(
       "#{Github::GITHUB_URL}/repos/#{repo}/issues?per_page=100&page=#{page}&state=all",
       headers: {
         Accept: 'application/vnd.github.v3+json"',
@@ -60,13 +59,20 @@ class Github
     )
   end
 
-  def self.fetch_meta(url)
-    response = HTTParty.get(
+  def self.fetch_meta(url, retries = 3)
+    HTTParty.get(
       "#{url}?per_page=100",
       headers: {
         Accept: 'application/vnd.github.v3+json"',
         Authorization: "token #{GITHUB_TOKEN}"
       }
     )
+  rescue Net::OpenTimeout => e
+    puts "TRY #{retries}/n ERROR: timed out while trying to connect #{e}"
+    raise if retries <= 1
+
+    puts 'Sleeping 3 and retrying'
+    sleep 3
+    Github.fetch_meta(url, retries - 1)
   end
 end
